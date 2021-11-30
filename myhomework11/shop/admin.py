7e4879ad6305e89628df6671d54dcf1e1e52e42a
namuns <skymsn214@gmail.com>
2021-11-30 18:06:40 +0900

    admin 명령 추가

--- myhomework11/shop/admin.py
+++ myhomework11/shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Shop
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ["Shop_title", "Shop_author_name", "Shop_created_at"]
+    search_fields = ["Shop_title"]
+
+
+admin.site.register(Shop, ShopAdmin)
