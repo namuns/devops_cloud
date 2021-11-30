37eb8d589dfbb1a9e48828acc4a52cc0e6ae92af
namuns <skymsn214@gmail.com>
2021-12-01 08:27:27 +0900

    templates생성 및 urls수정

--- myhomework10/market/views.py
+++ myhomework10/market/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    return render(request, "market/index.html")
