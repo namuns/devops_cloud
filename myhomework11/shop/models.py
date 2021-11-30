a97d44e57b59c45e8a0258f4b95482faa5428732
namuns <skymsn214@gmail.com>
2021-11-30 23:44:00 +0900

    list/detail 뷰/템플릿 생성

--- myhomework11/shop/models.py
+++ myhomework11/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from shop.upload_to import uuid_name_upload_to
 
 class Shop(models.Model):
     Shop_title = models.CharField(max_length=200, verbose_name="제목")
@@ -15,6 +16,6 @@ class Shop(models.Model):
     Shop_price = models.IntegerField(default=0, verbose_name="가격")
     Shop_content = models.TextField(verbose_name="본문")
     # upload_to에 문자열을 지정하면 파일이 저장되는 폴더의 경로가 된다.(파일 많을경우)
-    Shop_photo = models.ImageField(blank=True, verbose_name="사진")
+    Shop_photo = models.ImageField(blank=True, verbose_name="사진", upload_to=uuid_name_upload_to)
     Shop_created_at = models.DateTimeField(auto_now_add=True, verbose_name="작성시간") # 생성시각
     Shop_updated_at = models.DateTimeField(auto_now=True)
