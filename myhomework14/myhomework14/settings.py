5a02cb32bdbe330ec7d7a249cde18bb70bf5dbe9
namuns <skymsn214@gmail.com>
2021-12-02 18:22:52 +0900

    dogtube App 생성 및 등록

--- myhomework14/myhomework14/settings.py
+++ myhomework14/myhomework14/settings.py
@@ -17,12 +17,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+    # local apps
+    'dogtube',
+
 ]
 if DEBUG:
     INSTALLED_APPS += [
