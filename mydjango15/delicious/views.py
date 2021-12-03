05296cad374890568988975c118ceaec85c80b22
namuns <skymsn214@gmail.com>
2021-12-03 21:42:21 +0900

    맛집 web 예제

---
+++ mydjango15/delicious/views.py
@@ -0,0 +1,59 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+
+from delicious.forms import ShopForm
+from delicious.models import Shop
+
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+
+    # QueryString에 query이름의 인자를 확인
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(name__icontains=query)
+
+    # template_name = "delicious/shop_list.html"
+    context_data = {
+        "shop_list": qs,
+    }
+    return render(request, "delicious/shop_list.html", context_data)
+
+
+def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = Shop.objects.get(pk=pk)
+    # templates_name = "delicious/shop_detail.html"
+    context_data = {
+        "shop": shop,
+    }
+    return render(request, "delicious/shop_detail.html", context_data)
+
+
+def shop_new_1(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":  # "GET", "POST"
+        return render(request, "delicious/shop_form_1.html", {})
+    else:  # "POST"
+        name = request.POST["name"]
+        description = request.POST["description"]
+        address = request.POST["address"]
+        latitude = request.POST["latitude"]
+        longitude = request.POST["longitude"]
+        telephone = request.POST["telephone"]
+        # ToDo: 유효성 검사해야.
+        Shop.objects.create(
+            name=name,
+            description=description,
+            address=address,
+            latitude=latitude,
+            longitude=longitude,
+            telephone=telephone,
+        )
+        return redirect("/delicious")
+
+
+shop_new = CreateView.as_view(
+    model=Shop,
+    form_class=ShopForm,
+    success_url="/delicious/",
+)
