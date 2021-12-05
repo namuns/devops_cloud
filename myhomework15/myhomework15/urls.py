9143c5e55e26f799fcd67d6b874b73c5ee16a8f3
namuns <skymsn214@gmail.com>
2021-12-05 23:04:45 +0900

    Django 앱 생성 및 등록

--- myhomework15/myhomework15/urls.py
+++ myhomework15/myhomework15/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from covidclinic.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
