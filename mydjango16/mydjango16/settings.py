8fd5b138fca423b71b41c39c4a6cb4c67d389446
namuns <skymsn214@gmail.com>
2021-12-06 15:37:10 +0900

    blog app 생성 및 urls추가

--- mydjango16/mydjango16/settings.py
+++ mydjango16/mydjango16/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third apps
+    # local apps
+    'blog',
 ]
 
 if DEBUG:
