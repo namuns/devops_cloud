51c040a1de2f194bdc5aff0987e2cb5c5f27742b
namuns <skymsn214@gmail.com>
2021-12-06 17:06:05 +0900

    모델 1:N/M:N 관계 예시로서 모델 및 마이그레이션 추가

--- mydjango16/blog/admin.py
+++ mydjango16/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from blog.models import Post, Comment, Tag
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["pk", "post", "message", "created_at"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- mydjango16/blog/models.py
+++ mydjango16/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):  # pk: ID=(int)
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    tag_set = models.ManyToManyField('Tag', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # 인스턴스에 대한 문자열 title 표시를 기대하는 문법
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
