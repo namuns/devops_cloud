529887a1c4af182f3f5d0511d39334e7eb8e7842
namuns <skymsn214@gmail.com>
2021-12-07 01:55:49 +0900

    9. form을 활용한 검색기능 추가

--- myhomework16/covidclinic/views.py
+++ myhomework16/covidclinic/views.py
@@ -1,3 +1,4 @@
+from django.db.models import query
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from covidclinic.models import Clinic
@@ -5,6 +6,11 @@ from covidclinic.models import Clinic
 
 def clinic_list(request: HttpRequest) -> HttpResponse:
     qs = Clinic.objects.all()
+
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(name__icontains=query)
+
     context_data = {
         "clinic_list": qs,
     }
