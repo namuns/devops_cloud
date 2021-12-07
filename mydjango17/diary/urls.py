e9aeacd6c961aff92ec03b2b17c861a85a3fb4e1
namuns <skymsn214@gmail.com>
2021-12-07 11:46:56 +0900

    post_list/views 구현

--- mydjango17/diary/urls.py
+++ mydjango17/diary/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path
+
 from diary import views
 
 app_name = "diary"
 
-urlpatterns = []
-
-
+urlpatterns = [
+    path("", views.post_list, name="post_list"),
+]
--- mydjango17/diary/views.py
+++ mydjango17/diary/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from diary.models import Post
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    return render(request, "diary/post_list.html", {
+        "post_list": qs,
+    })
