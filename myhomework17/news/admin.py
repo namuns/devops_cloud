070f6ede3b49434931cfbe39f0ef42ba62b76ea0
namuns <skymsn214@gmail.com>
2021-12-08 15:11:39 +0900

    수정 사항 변경 및 정리

--- myhomework17/news/admin.py
+++ myhomework17/news/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from news.models import News, Comment, Tag
 
-# Register your models here.
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
