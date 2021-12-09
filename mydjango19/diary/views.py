0c4e1a2e2de04c61bf03a7f291f195dc98c59a91
namuns <skymsn214@gmail.com>
2021-12-09 12:29:33 +0900

    포스팅 쓰기 폼 2단계 구현

--- mydjango19/diary/views.py
+++ mydjango19/diary/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from diary.forms import PostForm
 from diary.models import Post
@@ -38,7 +38,26 @@ def tag_detail(request: HttpRequest, tag_name: str) -> HttpResponse:
 
 
 def post_new(request: HttpRequest) -> HttpResponse:
-    form = PostForm()
+    # print("request.method :", request.method)
+    # print("request.GET :", request.GET)
+    # print("request.POST :", request.POST)
+    # print("request.FILES :", request.FILES)
+
+    # 입력 서식을 보여주겠다.
+    if request.method == "GET":
+        form = PostForm()
+    # 서식 입력값을 전달받아서 유효성 검사를 하겠다.
+    # ->> 에러 상황에서는 에러메세지를 보여주겠다.
+    # ->> 유효성 검사에 통과하면 입력값을 보여주고 post_list로 이동
+    else:
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            print("유효성 검사에 통과했습니다. :", form.cleaned_data)
+            form.save() # ModelForm에서만 지원
+            return redirect("diary:post_list")
+        else:
+            pass
+
     return render(request, "diary/post_form.html", {
         "form": form,
     })
