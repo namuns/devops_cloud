73b8fb1783d0f77e80c188a6063abeed79a57ccb
namuns <skymsn214@gmail.com>
2021-12-10 17:47:36 +0900

    debug_toolbar 생성

--- mydjango20/mydjango20/settings.py
+++ mydjango20/mydjango20/settings.py
@@ -29,6 +29,13 @@ INSTALLED_APPS = [
     'shop',
 ]
 
+
+if DEBUG:
+    INSTALLED_APPS += [
+        "debug_toolbar",
+    ]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -38,6 +45,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+if DEBUG:
+    MIDDLEWARE = [
+                     "debug_toolbar.middleware.DebugToolbarMiddleware",
+                 ] + MIDDLEWARE
+
 
 ROOT_URLCONF = 'mydjango20.urls'
 
@@ -113,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
