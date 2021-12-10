6760764453dd9675ae130bbcbdb269b190a5ecae
namuns <skymsn214@gmail.com>
2021-12-10 17:13:23 +0900

    shop_new/shop_detail 뷰 구현

--- mydjango20/shop/urls.py
+++ mydjango20/shop/urls.py
@@ -1,2 +1,11 @@
+from django.urls import path
 
-urlpatterns = []
+from shop import views
+
+app_name = "shop"
+
+
+urlpatterns = [
+    path("<int:pk>/", views.shop_detail, name="shop_detail"),
+    path("new/", views.shop_new, name="shop_new"),
+]
