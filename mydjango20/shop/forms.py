86628480edd2676849066b728e88ca9c20e21f33
namuns <skymsn214@gmail.com>
2021-12-13 16:13:48 +0900

    shop_new 뷰에 구현된 Tag 저장 로직을 shopForm 클래스로 이동

--- mydjango20/shop/forms.py
+++ mydjango20/shop/forms.py
@@ -1,9 +1,27 @@
 from django import forms
-from shop.models import Shop, Review
+from shop.models import Shop, Review, Tag
 
 
 class ShopForm(forms.ModelForm):
     tags = forms.CharField()
+
+    def save(self):
+        # 부모의 save를 호출해주어야 합니다.
+        saved_post = super().save()
+
+        # 부모가 한 연산 + 부가적인 연산 수행 가능
+        tag_list = []
+        tags = self.cleaned_data.get("tags", "")
+        for word in tags.split(","):
+            tag_name = word.strip()
+            tag, __ = Tag.objects.get_or_create(name=tag_name)
+            tag_list.append(tag)
+
+            saved_post.tag_set.add(*tag_list)
+            saved_post.tag_set.clear()  # 간단 구현을 위해 clear 호출
+
+            return saved_post
+
     class Meta:
         model = Shop
         fields = [
