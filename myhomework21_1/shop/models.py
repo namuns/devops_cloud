a811d896bc2e7f2924c879e92b4384543051f520
namuns <skymsn214@gmail.com>
2021-12-13 20:43:14 +0900

    layout에 bootstrap5 적용 및 모델 만들고 admin에 등록

--- myhomework21_1/shop/models.py
+++ myhomework21_1/shop/models.py
@@ -1,2 +1,47 @@
-
+from django.core.validators import RegexValidator
 from django.db import models
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Shop(TimestampedModel):
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=14,
+                                 validators=[
+                                     RegexValidator(r"^\d{3}-?\d{4}-?\d{4}$", message="전화번호를 입력해주세요."),
+                                 ],
+                                 help_text="입력 예) 042-1234-1234")
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
