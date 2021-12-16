1fa4c6727359451d34d85f2e2a38831e2fcd49eb
namuns <skymsn214@gmail.com>
2021-12-17 00:54:11 +0900

    shop_list 구현

--- myhomework24/shop/urls.py
+++ myhomework24/shop/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
 
+from shop import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.shop_list, name="shop_list"),
+]
--- myhomework24/shop/views.py
+++ myhomework24/shop/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from shop.models import Shop
+
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+
+    qs = Shop.objects.all()
+    return render(request, "shop/shop_list.html", {
+        "shop_list": qs,
+    })
