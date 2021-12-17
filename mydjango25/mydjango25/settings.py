688611f5e3218c1924e5ce35f6150a8725e364d7
namuns <skymsn214@gmail.com>
2021-12-17 10:34:10 +0900

    bootstrap5추가

--- mydjango25/mydjango25/settings.py
+++ mydjango25/mydjango25/settings.py
@@ -21,12 +21,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # localapps
+
+    # thirdapps
+    'bootstrap5',
+
+
 ]
 
 if DEBUG:
