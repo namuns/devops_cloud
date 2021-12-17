8be3a1e6ccc4589b0a7773a9c9d6cb066b30aff5
namuns <skymsn214@gmail.com>
2021-12-17 16:49:06 +0900

    review_edit 뷰를 CBV로 구현

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, resolve_url
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from shop.forms import ReviewForm
+from shop.mixins import ReviewUserCheckMixin
 from shop.models import Shop, Category, Review
 
 
@@ -29,7 +30,8 @@ shop_detail = DetailView.as_view(
 class ReviewCreateView(LoginRequiredMixin, CreateView):
     model = Review
     form_class = ReviewForm
-    success_url = reverse_lazy("shop:shop_list")
+
+    # success_url = reverse_lazy("shop:shop_list")
 
     # 유효성 검사에 통과한다면? 호출되는 form_vaild
     def form_valid(self, form) -> HttpResponse:
@@ -44,5 +46,19 @@ class ReviewCreateView(LoginRequiredMixin, CreateView):
 
         return redirect(shop)
 
+
 review_new = ReviewCreateView.as_view(
 )
+
+
+class ReviewUpdateView(LoginRequiredMixin, ReviewUserCheckMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    # success_url = reverse_lazy("shop:shop_list")
+
+    def get_success_url(self) -> str:
+        review = self.object
+        return resolve_url(review.shop)
+
+
+review_edit = ReviewUpdateView.as_view()
