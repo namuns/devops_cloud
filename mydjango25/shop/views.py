035ca5dfa16a6909165ac7cc73035cd7581b8bc3
namuns <skymsn214@gmail.com>
2021-12-17 16:49:42 +0900

    shop_list 뷰에서 n개 단위로 페이징 처리

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -11,6 +11,7 @@ from shop.models import Shop, Category, Review
 
 class ShopListView(ListView):
     model = Shop
+    paginate_by = 4
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
