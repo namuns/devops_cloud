9855414acc6f0c0c901a8fb571246649cc68d121
namuns <skymsn214@gmail.com>
2021-12-23 16:58:28 +0900

    axios-jsonp 활용 Melon 검색 결과 연동

--- myreact29/src/MelonSearch.jsx
+++ myreact29/src/MelonSearch.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import { Input } from 'antd';
 
+import Axios from 'axios';
+import jsonpAdaptor from 'axios-jsonp';
+
 function MelonSearch() {
   const [query, setQuery] = useState('');
+  const [songList, setSongList] = useState([]);
 
   const handleChange = (e) => {
     const {
@@ -17,6 +21,34 @@ function MelonSearch() {
     console.group('handlePressEnter');
     console.log('검색어 ${query}(으)로 검색합니다.');
     console.groupEnd();
+
+    const url = 'https://www.melon.com/search/keyword/index.json';
+
+    Axios({
+      url: url,
+      adapter: jsonpAdaptor,
+      callbackParamName: 'jscallback',
+      params: {
+        query: query,
+      },
+    })
+      .then((response) => {
+        //ALBUMCONTENTS, ARTISTCONTENTS 가능
+        const {
+          data: { SONGCONTENTS: searchedSongList },
+        } = response;
+        console.group('멜론 검색결과');
+        console.log(response);
+        console.log(searchedSongList);
+        console.groupEnd();
+
+        setSongList(searchedSongList);
+      })
+      .catch((error) => {
+        console.group('멜론 검색 에러');
+        console.error(error);
+        console.groupEnd();
+      });
   };
 
   return (
@@ -28,6 +60,14 @@ function MelonSearch() {
         onChange={handleChange}
         onPressEnter={handlePressEnter}
       />
+      {songList.map((song) => {
+        return (
+          <div>
+            <img src={song.ALBUMIMG} />
+            {song.SONGNAME} by{song.ARTISTNAME}
+          </div>
+        );
+      })}
     </div>
   );
 }
