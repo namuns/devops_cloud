0125222e6677e7e4daede78b598188dae05fe043
namuns <skymsn214@gmail.com>
2021-12-28 14:32:51 +0900

    프로필 데이터를 json 파일로 빼고, App 컴포넌트에서 활용

--- myreact31/src/App.js
+++ myreact31/src/App.js
@@ -6,50 +6,27 @@ import profileImage1 from "./img/member1.jpg";
 import profileImage2 from "./img/member2.jpg";
 import profileImage3 from "./img/member3.jpg";
 import profileImage4 from "./img/member4.jpg";
-// import member from './Page/memberInfo.json'
+import members from "./Page/memberInfo.json";
 
 function App() {
   const [pageName, setPageName] = useState("member1");
 
   return (
     <div>
-      {pageName == "member1" ? (
-        <ProfileCard
-          name="Jane"
-          role="UI/UX INTERACTIVE DEVELOPER"
-          profileImage={profileImage1}
-          facebook_url="facebook.com/jane"
-          email="jane@gmail.com"
-          changePageName={setPageName}
-        />
-      ) : pageName == "member2" ? (
-        <ProfileCard
-          name="Kevin"
-          role="Jane"
-          profileImage={profileImage2}
-          facebook_url="member.facebook_url"
-          email="member.email"
-          changePageName={setPageName}
-        />
-      ) : pageName == "member3" ? (
-        <ProfileCard
-          name="John"
-          role="Jane"
-          profileImage={profileImage3}
-          facebook_url="member.facebook_url"
-          email="member.email"
-          changePageName={setPageName}
-        />
-      ) : (
-        <ProfileCard
-          name="qwerty"
-          role="Jane"
-          profileImage={profileImage4}
-          facebook_url="member.facebook_url"
-          email="member.email"
-          changePageName={setPageName}
-        />
-      )}
+      {members.map((member) => {
+        if (pageName === member.member) {
+          return (
+            <ProfileCard
+              name={member.name}
+              role={member.role}
+              facebook_url={member.facebook_url}
+              email={member.email}
+              profileImage={profileImage1}
+              changePageName={setPageName}
+            />
+          );
+        }
+      })}
 
       <PageLotto />
     </div>
