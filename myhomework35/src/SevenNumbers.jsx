412f1f23e0ae4bef280b4f7aadf0fb76a65112d1
namuns <skymsn214@gmail.com>
2022-01-03 21:51:35 +0900

    numbers, colors 초기값 설정

--- myhomework35/src/SevenNumbers.jsx
+++ myhomework35/src/SevenNumbers.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 
-function SevenNumbers({ initialValue }) {
-  const [] = useState([]);
-
+function reducer(prevState, action) {}
+function SevenNumbers() {
+  const [state, dispatch] = useReducer(reducer, {
+    numbers: [0, 0, 0, 0, 0, 0, 0],
+    colors: [
+      '#1B62BF',
+      '#1755A6',
+      '#37A647',
+      '#F29F05',
+      '#F23838',
+      'purple',
+      'pink',
+    ],
+  });
   return (
     <div>
-      <h2>Number</h2>
-
+      <h2>Number7</h2>
       <hr />
     </div>
   );
