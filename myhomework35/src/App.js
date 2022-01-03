747194207f61154be1cfed173477ffb6acd9f131
namuns <skymsn214@gmail.com>
2022-01-03 18:31:32 +0900

    최소 버전 구현

---
+++ myhomework35/src/App.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import SevenNumbers from './SevenNumbers';
+
+function App() {
+  return <SevenNumbers />;
+}
+
+export default App;
---
+++ myhomework35/src/SevenNumbers.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function SevenNumbers({ initialValue }) {
+  const [] = useState([]);
+
+  return (
+    <div>
+      <h2>Number</h2>
+
+      <hr />
+    </div>
+  );
+}
+
+export default SevenNumbers;
