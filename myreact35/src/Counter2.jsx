5b4ca2b3628b8d4bbedf80978c3d24ce6749f48f
namuns <skymsn214@gmail.com>
2022-01-03 14:29:45 +0900

    setValue/setColor에 함수를 넘겨 상탯값을 변경

--- myreact35/src/Counter2.jsx
+++ myreact35/src/Counter2.jsx
@@ -1,75 +1,57 @@
 import { useState } from "react";
 
-// { type: "PLUS", amount: 1 }
-// { type: "MINUS", amount: 1 }
-function dispatch(action, prevState) {
-  const { type, amount } = action;
-  if (type === "PLUS") {
-    return prevState + amount;
-  } else if (type === "MINUS") {
-    return prevState - amount;
-  }
-  // 버그 !!!
-  return prevState;
-}
-
-function Counter2() {
-  // TODO: color와 value
+function Counter() {
   const [value, setValue] = useState(0);
+  const [color, setColor] = useState("red");
 
   const handlePlus = () => {
-    const action = { type: "PLUS", amount: 1 };
-    setValue((prevValue) => {
-      // dispatch 함수를 호출하여, 새로운 상탯값을 계산해냅니다.
-      return dispatch(action, prevValue);
-    });
+    // setValue(value + 1);
+    setValue((prevValue) => prevValue + 1);
   };
 
   const handleMinus = () => {
-    const action = { type: "MINUS", amount: 1 };
-    setValue((prevValue) => {
-      return dispatch(action, prevValue);
-    });
+    // setValue(value - 1);
+    setValue((prevValue) => prevValue - 1);
   };
 
+  const handleGreen = () => {
+    //setColor("green");
+    setColor(() => "green");
+  };
+
+  const handleBlue = () => {
+    //setColor("blue");
+    setColor(() => "blue");
+  };
+
+  const handleRed = () => {
+    //setColor("red");
+    setColor(() => "Red");
+  };
   return (
     <div>
-      <h2>Counter2</h2>
-      {value}
-      <hr />
-      <button onClick={handlePlus}>증가</button>
-      <button onClick={handleMinus}>감소</button>
+      <h2>Counter</h2>
+      <div style={{ ...defaultStyle, backgroundColor: color }}>{value}</div>
+      <h3>
+        <button onClick={handlePlus}>+</button>
+        <button onClick={handleMinus}>-</button>
+        <button onClick={handleGreen}>green</button>
+        <button onClick={handleBlue}>blue</button>
+        <button onClick={handleRed}>red</button>
+      </h3>
     </div>
   );
 }
 
-export default Counter2;
+const defaultStyle = {
+  width: "100px",
+  height: "100px",
+  borderRadius: "50px",
+  lineHeight: "100px",
+  textAlign: "center",
+  display: "inline-block",
+  fontSize: "3rem",
+  userSelect: "none",
+};
 
-// 1. 새로운 상탯값 color, setColor을 정의
-//  - 초기값 : "red"
-// 2. 3개의 버튼 "색상 변경"
-//  - 1번 버튼 클릭하면 : green => { action: "CHANGE_COLOR", color: "green"}
-//  - 2번 버튼 클릭하면 : blue => { action: "CHANGE_COLOR", color: "blue"}
-//  - 3번 버튼 클릭하면 : red => { action: "CHANGE_COLOR", color: "red"}
-// 3. 새로운 dispatch 함수인 dispatch_color를 구현
-
-// function dispatch(action, state) {
-//     const { type, payload } = action;
-//     if (type === "INCREMENT") {
-//       const { value } = payload;
-//       return {
-//         ...state,
-//         value: state.value + value,
-//       };
-//     } else {
-//       return state;
-//     }
-//   }
-
-//   const action = { type: "INCREMENT", payload: { value: 1 } };
-//   dispatch(action);
-
-//   dispatch({
-//     type: "INCREMENT",
-//     payload: { value: 3 },
-//   });
+export default Counter;
